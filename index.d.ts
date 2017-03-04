4dc80d68b1cbc55b27e727d041c6948783f9929e
Marc Neumann <marc.nemn@gmail.com>
2017-03-04 15:59:16 +0100

    Various fixes.

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export {ClientOptionBuilder} from './src/connection/ClientOptionBuilder';
-export {ComponentOptionBuilder} from './src/connection/ComponentOptionBuilder';
-export {Connection, MessageFilter} from './src/connection/Connection';
-export {ConnectionBuilder} from './src/connection/ConnectionBuilder';
-export {SimpleStanza} from './src/connection/SimpleStanza';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export {ClientOptionBuilder} from './connection/ClientOptionBuilder';
+export {ComponentOptionBuilder} from './connection/ComponentOptionBuilder';
+export {Connection, MessageFilter} from './connection/Connection';
+export {ConnectionBuilder} from './connection/ConnectionBuilder';
+export {SimpleStanza} from './connection/SimpleStanza';
